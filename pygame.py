a2916b6db8077af9594444370310b762c017a9a3
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-10-09 20:37:34 +0530

    submit

---
+++ pygame.py
@@ -0,0 +1,19 @@
+#import necessary libraries
+import pygame
+#initialize required modules
+pygame.init()
+# setup window geometry
+screen = pygame.display.set_mode((400,500))
+#create a loop to run till the game is quit by the user
+done = False
+while not done:
+    #clear the event queue
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            #CORRECTED:Set 'done to True to exit the while loop
+            done = True
+        #make the changes visible
+        pygame.display.flip()
+
+#after the loop exits,safely uninitialize pygame
+pygame.quit()
